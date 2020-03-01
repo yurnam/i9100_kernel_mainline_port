49f32d4eb77788454bea048a023400b4d82f81ff
yurnam <m.kritzer@gmx.at>
2020-03-01 21:03:09 +0100

    update defconfig

--- build.sh
+++ build.sh
@@ -25,23 +25,21 @@ make -j$(nproc)
 cat arch/arm/boot/zImage arch/arm/boot/dts/$DTB > $COMB_ZIMAGE
 mkimage -A arm -O linux -T kernel -C none -a 0x40008000 -e 0x40008000 -d $COMB_ZIMAGE ../images/recovery.img
 
-dd if=/dev/zero of=./modules.img bs=1M count=100
-mkfs.ext4 modules.img
+#dd if=/dev/zero of=./modules.img bs=1M count=50
+#mkfs.ext4 modules.img
 rm -rf MODTEMP
 mkdir MODTEMP
-mount modules.img MODTEMP
+#mount modules.img MODTEMP
 make modules_install INSTALL_MOD_PATH=./MODTEMP INSTALL_MOD_STRIP=1
 make modules_install INSTALL_MOD_PATH=../rootfs/ INSTALL_MOD_STRIP=1
 
 if [ -d "MODTEMP/lib/modules" ]; then
-	cd MODTEMP/lib/modules
-	mv * ../../
-	cd ../../
-	rm -rf lib
-	cd ..
-	umount MODTEMP
+	mv MODTEMP/lib/modules/$(ls MODTEMP/lib/modules/)/* MODTEMP/
+	rm -rf MODTEMP/lib
+	#umount MODTEMP
+	make_ext4fs -s -l 200M modules.img MODTEMP	
 	mv modules.img ../images/
-
+	
 
 fi
 
